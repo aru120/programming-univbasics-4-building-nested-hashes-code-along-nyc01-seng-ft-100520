be8c8000269ccbdeda6fff803f9a86fa4b030fa6
Kevin Huang <khuang159@gmail.com>
2020-09-14 16:18:14 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -5,22 +5,22 @@ def third_challenge
        
         },
       :matriarch => {
-        :name: "Lady Montague", :age =>
+        :name: "Lady Montague", :age => "54"
         },
       :hero => {
-       
+       :name => "Romeo", :age => "15", :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-      
+      :name: "Lord Capulet", :age => "50"
         },
       :matriarch => {
-      
+      :name: "Lady Capulet", :age => "51"
         },
       :heroine => {
-       
+       "Juliet", :age => "15", :status => "alive"
         },
       :heroine_friends => []
    }
